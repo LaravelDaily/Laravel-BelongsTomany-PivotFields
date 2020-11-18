7ba7985127766a65a060c3c6d7b6ed5ec6683e3b
Karolis <narkevicius.karolis@gmail.com>
2020-11-18 21:23:22 +0200

    remove apline, add jquery

--- resources/views/admin/recipes/partials/ingredients.blade.php
+++ resources/views/admin/recipes/partials/ingredients.blade.php
@@ -1,15 +1,23 @@
 <table>
     @foreach($ingredients as $ingredient)
-        <tr x-data="{enabled: {{ $ingredient->value ? 'true' : 'false' }}, value: {{ $ingredient->value ?? 'null' }}}">
-            <td>
-                <input x-model="enabled" @change="value = null" type="checkbox">
-            </td>
-            <td @click="enabled = !enabled" style="cursor:pointer;">
-                {{ $ingredient->name }}
-            </td>
-            <td>
-                <input x-model="value" :disabled="!enabled" name="ingredients[{{ $ingredient->id }}]" type="text" class="form-control" placeholder="Amount">
-            </td>
+        <tr>
+            <td><input {{ $ingredient->value ? 'checked' : null }} data-id="{{ $ingredient->id }}" type="checkbox" class="ingredient-enable"></td>
+            <td>{{ $ingredient->name }}</td>
+            <td><input value="{{ $ingredient->value ?? null }}" {{ $ingredient->value ? null : 'disabled' }} data-id="{{ $ingredient->id }}" name="ingredients[{{ $ingredient->id }}]" type="text" class="ingredient-amount form-control" placeholder="Amount"></td>
         </tr>
     @endforeach
 </table>
+
+@section('scripts')
+    @parent
+    <script>
+        $('document').ready(function () {
+            $('.ingredient-enable').on('click', function () {
+                let id = $(this).attr('data-id')
+                let enabled = $(this).is(":checked")
+                $('.ingredient-amount[data-id="' + id + '"]').attr('disabled', !enabled)
+                $('.ingredient-amount[data-id="' + id + '"]').val(null)
+            })
+        });
+    </script>
+@endsection
